6d68c7bf14c4d571133d865801f01fa02851d1d7
asirimanjitha <asirisenith@gmail.com>
2023-10-30 19:03:00 +0530

    created WalksController

---
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+    }
+}
