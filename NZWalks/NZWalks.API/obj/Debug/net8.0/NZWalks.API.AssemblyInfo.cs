3cd3e17865ce69e666fe9ea3e161f8b8e8df21b0
AsiriZenith <asirisenith@gmail.com>
2025-05-24 11:17:46 +0530

    remove unnecessary file from git changes

--- NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c9e962374675ff16a8bcaf16ec0f6ee738c96a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4ec74216bea885386d7144b4776787febaf899")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
